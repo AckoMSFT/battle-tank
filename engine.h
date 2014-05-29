df4349be42fb79920dba244c74d115d66acf1811
Radenko Pavlovic <radenkop@yahoo.com>
2014-05-29 21:29:49 +0200

    Fixed engine

--- engine.h
+++ engine.h
@@ -4,6 +4,7 @@
 void go_up( Tank tank )
 {
     int up = tank.x - 1;
+    int x = tank.x, y = tank.y;
     if ( ( map[ up ][ y ] == EMPTY ) && ( map[ up ][ y + 1 ] == EMPTY ) && ( map[ up ][ y + 2 ] == EMPTY ) )
         if ( tank.dir == UP )
         {
@@ -22,6 +23,7 @@ void go_up( Tank tank )
 void go_down( Tank tank )
 {
     int down = tank.x + 3;
+    int x = tank.x, y = tank.y;
     if ( ( map[ down ][ y ] == EMPTY ) && ( map[ down ][ y + 1 ] == EMPTY ) && ( map[ down ][ y + 2 ] == EMPTY ) )
         if ( tank.dir == DOWN )
         {
@@ -40,6 +42,7 @@ void go_down( Tank tank )
 void go_left( Tank tank )
 {
     int left = tank.y - 1;
+    int x = tank.x, y = tank.y;
     if ( ( map[ x ][ left ] == EMPTY ) && ( map[ x + 1 ][ left ] == EMPTY ) && ( map[ x + 2 ][ left ] == EMPTY ) )
         if ( tank.dir == LEFT )
         {
@@ -58,18 +61,19 @@ void go_left( Tank tank )
 void go_right( Tank tank )
 {
     int right = tank.y + 3;
+    int x = tank.x, y = tank.y;
     if ( ( map[ x ][ right ] == EMPTY ) && ( map[ x + 1 ][ right ] == EMPTY ) && ( map[ x + 2 ][ right ] == EMPTY ) )
         if ( tank.dir == RIGHT )
         {
             erase_tank( tank.x, tank.y );
             tank.y = right;
-            print_tank( tank.x, tank.y );
+            print_tank( tank.dir, tank.x, tank.y );
         }
         else
         {
             tank.dir = RIGHT;
             print_tank( tank.dir, tank.x, tank.y );
-            tank.y = left;
+            tank.y = right;
             print_tank( tank.dir, tank.x, tank.y );
         }
 }
@@ -98,13 +102,35 @@ void update_game()
                 erase_bullet( bullets[ i ]->x, bullets[ i ]->y );
                 switch ( bullets[ i ]->dir )
                 {
-                    case UP: { bullets[ i ]->x--; print_bullet( bullets[ i ]->x, bullets[ i ]->y ); break; }
-                    case DOWN: { bullets[ i ]->x++; print_bullet( bullets[ i ]->x, bullets[ i ]->y ); break; }
-                    case LEFT: { bullets[ i ]->y--; print_bullet( bullets[ i ]->x, bullets[ i ]->y ); break; }
-                    case RIGHT: { bullets[ i ]->y++; print_bullet( bullets[ i ]->x, bullets[ i ]->y ); break; }
+                    case UP: { bullets[ i ]->x--; break; }
+                    case DOWN: { bullets[ i ]->x++; break; }
+                    case LEFT: { bullets[ i ]->y--; break; }
+                    case RIGHT: { bullets[ i ]->y++; break; }
                 }
             }
-            if ( map[ bullets[ i ]->x ][ bullets[ i ]->y ] == 'T' )
+            switch ( map[ bullets[ i ]->x ][ bullets[ i ]->y ] )
+            {
+                case 'T':
+                    p = grid[ bullets[ i ]->x ][ bullets[ i ]->y ];
+                    erase_tank( p->x, p->y );
+                    free( p );
+                    p = NULL;
+                    erase_bullet( p->x, p->y );
+                    free( bullets[ i ] );
+                    bullets[ i ] = NULL;
+                    break;
+                case 'B':
+                    p = grid[ bullets[ i ]->x ][ bullets[ i ]->y ];
+                    print_empty( p->x, p->y );
+                    free( p );
+                    p = NULL;
+                    free( bullets[ i ] );
+                    bullets[ i ] = NULL;
+                    break;
+                case 'S':
+                    p = grid[ bullets[ i ]->x ][ bullets[ i ]->y ];
+
+
 
 
         }
