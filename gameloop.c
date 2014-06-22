cbd5493801963c2230a2641f6d978bc27af29b34
Aleksandar Ivanović <aleksandar.ivanovic.94@gmail.com>
2014-06-22 15:34:33 +0200

    asd

--- gameloop.c
+++ gameloop.c
@@ -93,6 +93,7 @@ void startGame(int difficulty)
             update_high_scores("al3ksandar",score);
             return;
         }
+        else print_end_level_screen ( i );
     }
     kill_curses();
     puts("osvojio si");
@@ -104,6 +105,7 @@ void startGame(int difficulty)
 bool startLevel(int level)
 {
     print_start_level_screen(level);
+    sound_start_music();
     // give me current level
     char level_name[1 << 5], buffer[1 << 5];
     strcpy(level_name,"level");
@@ -111,7 +113,6 @@ bool startLevel(int level)
     strcat(level_name, buffer);
     strcat(level_name, ".map");
     load_map(level_name);
-    sound_start_music();
 
 
     int i,j,keyPressed,enemySpawn,x,y,enemyChoice,di,dj;
