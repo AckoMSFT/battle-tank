b58c2dc62ba5053c68ba247a6f1da3767aff7f64
Bojan Rosko <znjrols5@gmail.com>
2014-06-25 13:09:00 +0200

    blaaah

--- make-me.bat
+++ make-me.bat
@@ -1,1 +1,1 @@
-gcc -o battle_tanks.exe ai.c editor.c engine.c highscores.c gameloop.c kletve.c linux_spec.c main.c menu.c queue.c sound.c stack.c -lpdcurses -lwinmm -w
+gcc -o battle_tanks.exe ai.c editor.c engine.c highscores.c gameloop.c kletve.c linux_spec.c main.c menu.c queue.c sound.c stack.c rsa.c -lpdcurses -lwinmm -w
